91bbfb8c5528cd0185003093e07d26054c33d57d
brnavelar <111910980+brnavelar@users.noreply.github.com>
2023-03-31 20:24:53 -0300

    Add files via upload

---
+++ Aula10_31_03_2021/Aula10_31_03_2021/TestaConjunto.java
@@ -0,0 +1,13 @@
+
+public class TestaConjunto {
+	public static void main(String [] args) {
+		Conjunto c = new Conjunto();
+		c.adicionar(2);
+		c.adicionar(4);
+		c.adicionar(3);
+		c.imprime();
+		System.out.println("Posição do número 4: " +c.busca(4));
+		c.remove(2);
+		c.imprime();
+	}
+}
